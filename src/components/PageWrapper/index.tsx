1f2eda06efab2a459f9496d061b3545f86f2a536
huangbh <578092215@qq.com>
2024-06-13 16:57:34 +0800

    style(component): 样式优化

--- src/components/PageWrapper/index.tsx
+++ src/components/PageWrapper/index.tsx
@@ -1,12 +1,17 @@
 import { NScrollbar } from 'naive-ui';
-import styles from './index.module.scss';
 
 export const PageWrapper = defineComponent({
   props: { useScrollbar: { type: Boolean, default: true } },
   setup(props, { slots }) {
     return () => (
-      <div class={['rounded-2xl w-full h-full min-h-full', styles['page-wrapper']]}>
-        {props.useScrollbar ? <NScrollbar class='rounded-2xl'>{slots.default?.()}</NScrollbar> : slots.default?.()}
+      <div class='rounded-2xl w-full h-full min-h-full'>
+        {props.useScrollbar ? (
+          <NScrollbar contentClass='px-8px' class='rounded-2xl'>
+            {slots.default?.()}
+          </NScrollbar>
+        ) : (
+          slots.default?.()
+        )}
       </div>
     );
   },
