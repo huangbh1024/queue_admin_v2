31caeca6543fd4739f0e4eb9ff0d8e1dc666cca2
huangbh <578092215@qq.com>
2024-06-28 09:14:23 +0800

    build: 打包优化

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,35 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import UnoCSS from 'unocss/vite';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import AutoImport from 'unplugin-auto-import/vite';
+import { vitePluginBuildFlow } from 'vite-plugin-build-flow';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx({ transformOn: true, mergeProps: true, enableObjectSlots: true }),
-    UnoCSS(),
-    AutoImport({ imports: ['vue', 'vue-router', 'pinia', '@vueuse/core'] }),
-  ],
-  base: './',
-  resolve: {
-    alias: [{ find: '@', replacement: '/src' }],
-  },
-});
+export default ({ mode }: { mode: string }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return defineConfig({
+    plugins: [
+      vue(),
+      vueJsx({ transformOn: true, mergeProps: true, enableObjectSlots: true }),
+      UnoCSS(),
+      AutoImport({ imports: ['vue', 'vue-router', 'pinia', '@vueuse/core'] }),
+      vitePluginBuildFlow({ version: process.env.VITE_PROJECT_VERSION ?? '' }),
+    ],
+    base: './',
+    resolve: {
+      alias: [{ find: '@', replacement: '/src' }],
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            lodash: ['lodash-es'],
+            naiveui: ['naive-ui'],
+            vue: ['vue'],
+          },
+        },
+      },
+    },
+  });
+};
